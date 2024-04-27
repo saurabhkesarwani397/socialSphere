fa777f57e64590973b9d3341c7f99835cd713595
saurabhkesarwani397 <168301095+saurabhkesarwani397@users.noreply.github.com>
2024-04-28 00:10:10 +0530

    Add files via upload

---
+++ client/src/components/Footer.js
@@ -0,0 +1,18 @@
+import { Card, Grid, Link, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import Copyright from "./Copyright";
+
+const Footer = () => {
+  return (
+    <Box pb={3}>
+      <Card>
+        <Typography variant="subtitle1">
+         Major Project by team - Social Sphere⭐
+        </Typography>
+      </Card>
+    </Box>
+  );
+};
+
+export default Footer;
