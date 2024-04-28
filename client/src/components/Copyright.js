0a26217ec0099d28c1fa96309db6dd32fce0f2fd
saurabhkesarwani397 <168301095+saurabhkesarwani397@users.noreply.github.com>
2024-04-28 14:26:55 +0530

    Update Copyright.js

--- client/src/components/Copyright.js
+++ client/src/components/Copyright.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Copyright = () => {
   return (
     <Typography variant="subtitle1" color="text.secondary">
-      Copyright © 2022{" "}
+      Copyright © 2024{" "}
       <Link to="/" color="inherit">
       Social Sphere
       </Link>
